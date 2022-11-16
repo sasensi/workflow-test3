01a35cf23208417fbb3dc2dda65e86c359258a31
sasensi <asensi.samuel@gmail.com>
2022-11-16 10:18:35 +0100

    fix action

--- .github/actions/hello-world/index.js
+++ .github/actions/hello-world/index.js
@@ -3,7 +3,7 @@ const github = require('@actions/github');
 
 try {
   const lastCommit = [...github.context.payload.commits].pop()
-  const isRelease = lastCommit.startsWith('chore(release): ');
+  const isRelease = lastCommit.message.startsWith('chore(release): ');
   core.setOutput('isRelease', isRelease);
   console.log(`Is release ? ${isRelease ? 'yes' : 'no'}`);
 } catch (error) {
